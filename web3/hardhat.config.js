659c38ece8af327aeabb29435b8cb4c66a1c6047
Jasveen <jazndj2@gmail.com>
2022-05-06 14:47:47 +0530

    Add files via upload

---
+++ web3/hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-typechain");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.0",
+};
